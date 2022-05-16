3a8b305b2255e9a03544e191e595b27f9e06be68
Guilherme Santana <g.santossantana22@gmail.com>
2022-05-16 09:10:47 -0300

    fabricantes

---
+++ fabricantes/listar.php
@@ -0,0 +1,44 @@
+<?php
+// SCRITP DE CONEXÃO AO SERVIDOR BANCO DE DADOS
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas";
+
+/* Criando a conexão com o MySQL (API/ Driver de conexão)*/
+$conexao = new PDO(
+    "mysql:host=$servidor; dbname=$banco; charset=utf8",
+    $usuario,
+    $senha
+);
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Lista </title>
+</head>
+<body>
+    <div class="container">
+        <h1>Fabricantes | SELECT</h1>
+        <hr>
+        <h2>Lendo e carregando todos os fabricantes</h2>
+         <table>
+             <caption>Lista de Fabricantes</caption>
+             <thead>
+                 <tr>
+                     <th>ID</th>
+                     <th>Nome</th>
+                 </tr>
+             </thead>
+             <tbody>
+
+             </tbody>
+         </table>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,13 +7,14 @@
     <title>CRUD Vendas</title>
 </head>
 <body>
+
     <div class="container">
-        <h1>CRUD - Sistemoa Vendas</h1>
+        <h1>CRUD - Sistema Vendas</h1>
         <hr>
         <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
 
         <ul>
-            <li><a href="">Fabricantes</a></li>
+            <li><a href="fabricantes/listar.php">Fabricantes</a></li>
             <li><a href="">Produtos</a></li>
         </ul>
     </div>
