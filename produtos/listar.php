b8876c02f0e7ed80fa5be2cf8150ee267621ecfa
Guilherme Santana <g.santossantana22@gmail.com>
2022-05-25 08:26:12 -0300

    criação da aba produtos

---
+++ produtos/listar.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos - Lista </title>
+</head>
+<body>
+    <div class="container">
+        <h1>Produtos | SELECT</h1> 
+        <a href="../index.php">HOME</a>
+        <hr>
+        <h2>Lendo e carregando todos os produtos</h2>
+        <p><a href="inserir.php">Inserir um novo produto</a></p>
+    </div>
+
+</body>
+</html>
