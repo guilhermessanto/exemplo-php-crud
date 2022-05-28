63893fc346ec8eb152c5233fc49241057df99140
Guilherme Santana <g.santossantana22@gmail.com>
2022-05-28 17:12:20 -0300

    botão excluir

--- produtos/listar.php
+++ produtos/listar.php
@@ -36,6 +36,15 @@ $listaDeProdutos = lerProdutos($conexao);
            <?php }?>
         </div>
     </div>
-
+<script>
+    let links = document.querySelectorAll('.excluir');
+    for(let i = 0; i < links.length; i++ ){
+        links[i].addEventListener("click", function(event){
+            event.preventDefault();
+            let resposta = confirm("Deseja realmente excluir?");
+            if(resposta){location.href = links[i].getAttribute('href');};
+        });
+    }
+</script>
 </body>
 </html>
--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -59,7 +59,17 @@ function atualizarProduto(PDO $conexao,int $id, string $nome, float $preco, int 
 }
 
 
+function excluirProduto(PDO $conexao, int $id):void{
+    $sql = "DELETE FROM produtos WHERE id = :id";
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta -> bindParam(':id',$id,PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro: ".$erro->getMessage());
+    }
 
+}
 
 // Funções utilitárias
 function dump($dados){
