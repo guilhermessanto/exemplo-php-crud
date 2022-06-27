1bba5f13860c4fccebdee8845d5c1280bbbce756
Guilherme Santana <g.santossantana22@gmail.com>
2022-06-27 09:21:14 -0300

    finalização conexão POO
    conexão com o banco de dados finalizado

--- src/Banco.php
+++ src/Banco.php
@@ -1,10 +1,32 @@
 <?php
 namespace CrudPoo;
+use PDO, Exception;
+
 abstract class Banco {
     /* Proppriedades ou atributos de acesso ao servidor de banco de dados*/
     private static string  $servidor = "localhost";
     private static string  $usuario = "root";
     private static string  $senha = "";
     private static string  $banco = "vendas";
+    /* private static \PDO $conexao;  Não precisa do use PDO*/
     private static PDO $conexao;
+
+    /* Método de conexão ao banco */
+
+    public static function conecta():PDO 
+    {
+        try{
+            self::$conexao = new PDO(
+                "mysql:host=".self::$servidor."; 
+                dbname=".self::$banco."; 
+                charset=utf8",
+                self::$usuario, /* self:: da acesso recursos statics dentro da classe */
+                self::$senha
+            );
+            self::$conexao ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
+        }catch(Exception $erro){
+            die("Deu ruim: ".$erro->getMessage());
+        }
+        return self::$conexao;
+    }
 }
