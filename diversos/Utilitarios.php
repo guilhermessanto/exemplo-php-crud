23142e694f9861909eb10832e9c173b30a9b6167
Guilherme Santana <g.santossantana22@gmail.com>
2022-07-20 08:38:58 -0300

    reinstalação vendor

--- diversos/Utilitarios.php
+++ diversos/Utilitarios.php
@@ -1,15 +1,38 @@
 <?php
 namespace utilitario;
-abstract class Utilitarios{
-    
-        public static function trataMoeda(float $valor):string {
-            return "R$ ".number_format($valor, 2, ",", ".");
-        }
-    
-        public static function teste(array $dados) {
-            echo "<pre>";
-            var_dump($dados);
-            echo "</pre>";
-        }
-    
-    }
+
+final class Utilitarios {
+    private float $valor;
+public function formataMoeda():string{
+    return "R$: ". number_format($this->valor,2, ',','.');   
+}
+
+
+// Funções utilitárias
+/* function dump($dados){
+echo "<pre>";
+var_dump($dados);
+echo "</pre>";
+} */
+
+public function getValor(): float
+{
+    return $this->valor;
+}
+
+public function setValor(float $valor)
+{
+    $this->valor = $valor;
+}
+}
+
+
+/* abstract class Utilitarios {
+public static function formataMoeda(float $valor):string{
+    return "R$: ". number_format($this->valor,2, ',','.');   
+}
+public static function dump(array $dados){
+echo "<pre>";
+var_dump($dados);
+echo "</pre>";
+} */
